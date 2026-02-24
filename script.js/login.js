2fbc0c4e2cdef0a254ae06acc435bec28e0efc76
abutalha08 <taufique752@gmail.com>
2026-02-24 18:41:44 +0600

    give functionality in login page and create homepage cards or buttons

--- script.js/login.js
+++ script.js/login.js
@@ -1,1 +1,26 @@
-console.log("Login functionality coming")
+console.log("Login functionality coming");
+
+document.getElementById("login-btn").addEventListener('click',function(){
+    console.log("login button clicked");
+    //1.Get the mobile number input
+    const numberInput = document.getElementById('input-mobile-number');
+    const contactNumber = numberInput.value;
+    console.log(contactNumber);
+    //2.Get the pin input
+    const inputPin = document.getElementById("input-pin-number");
+    const pin = inputPin.value;
+    console.log(pin);
+    //3.Match pin & mobile number
+    if(contactNumber == "01234567890" && pin == "1234"){
+        //3-1. true::>> alert > homepage
+        alert("Login success");
+
+        // window.location.replace("/homepage.html");
+        window.location.assign("/homepage.html");
+    }else{
+        //3-2. false::>> alert > return
+        alert("Login failed");
+        return;
+
+    }
+})
