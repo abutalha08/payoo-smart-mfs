051da5b31df767ace30368c7a9d3e445742fc414
abutalha08 <taufique752@gmail.com>
2026-02-26 23:46:59 +0600

    give toggling functionality in add money and cashout buttons

--- script.js/machine.js
+++ script.js/machine.js
@@ -20,3 +20,19 @@ function setBalance(value) {
   const CurrentBalanceInput = document.getElementById("current-balance");
   CurrentBalanceInput.innerText = value;
 }
+
+// Machine take e id -> hide all -> show the only given id section
+
+function showOnly(id) {
+  const addMoney = document.getElementById("add-money");
+  const cashOut = document.getElementById("cashOut");
+  console.log(`Add money- ${addMoney}, Cashout- ${cashOut}`);
+
+  // Hide everyone
+  addMoney.classList.add("hidden");
+  cashOut.classList.add("hidden");
+
+  // id wala element ta k show koro
+  const selected = document.getElementById(id);
+  selected.classList.remove("hidden");
+}
